15c2a60615582dfd5c24f790ba46ba483344601e
snehaaa-16 <snehajaiswal1607@gmail.com>
2026-03-16 22:03:19 +0530

    controller and service added

---
+++ src/main/java/com/sneha/studentmanagementapi/controller/StudentController.java
@@ -0,0 +1,28 @@
+package com.sneha.studentmanagementapi.controller;
+
+import com.sneha.studentmanagementapi.model.Student;
+import com.sneha.studentmanagementapi.service.StudentService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    private final StudentService service;
+
+    public StudentController(StudentService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public Student createStudent(@RequestBody Student student) {
+        return service.saveStudent(student);
+    }
+
+    @GetMapping
+    public List<Student> getStudents() {
+        return service.getAllStudents();
+    }
+}
---
+++ src/main/java/com/sneha/studentmanagementapi/service/StudentService.java
@@ -0,0 +1,25 @@
+package com.sneha.studentmanagementapi.service;
+
+import com.sneha.studentmanagementapi.model.Student;
+import com.sneha.studentmanagementapi.repository.StudentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    private final StudentRepository repository;
+
+    public StudentService(StudentRepository repository) {
+        this.repository = repository;
+    }
+
+    public Student saveStudent(Student student) {
+        return repository.save(student);
+    }
+
+    public List<Student> getAllStudents() {
+        return repository.findAll();
+    }
+}
