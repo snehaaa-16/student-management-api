eea46d9d52ca7f3aeaf89ff37809823a8af88f44
snehaaa-16 <snehajaiswal1607@gmail.com>
2026-03-17 23:05:26 +0530

    added basic auth

---
+++ src/main/java/com/sneha/studentmanagementapi/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.sneha.studentmanagementapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/auth/**",
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
+
+                .formLogin(form -> form.disable())
+                .httpBasic(httpBasic -> {});
+
+        return http.build();
+    }
+}
