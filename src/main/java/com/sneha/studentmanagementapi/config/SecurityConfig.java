8bdde22eec02c9fd25dbd857787fb8a9695bde58
snehaaa-16 <snehajaiswal1607@gmail.com>
2026-03-18 20:08:45 +0530

    implement JWT authentication filter

--- src/main/java/com/sneha/studentmanagementapi/config/SecurityConfig.java
+++ src/main/java/com/sneha/studentmanagementapi/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.sneha.studentmanagementapi.config;
 
+import com.sneha.studentmanagementapi.security.JwtFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,6 +8,12 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    private final JwtFilter jwtFilter;
+
+    public SecurityConfig(JwtFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -23,8 +30,10 @@ public class SecurityConfig {
                         .anyRequest().authenticated()
                 )
 
+                .addFilterBefore(jwtFilter, org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter.class)
+
                 .formLogin(form -> form.disable())
-                .httpBasic(httpBasic -> {});
+                .httpBasic(httpBasic -> httpBasic.disable());
 
         return http.build();
     }
---
+++ src/main/java/com/sneha/studentmanagementapi/security/JwtFilter.java
@@ -0,0 +1,45 @@
+package com.sneha.studentmanagementapi.security;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class JwtFilter implements Filter {
+
+    private final JwtUtil jwtUtil;
+
+    public JwtFilter(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    public void doFilter(ServletRequest request,
+                         ServletResponse response,
+                         FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        String authHeader = req.getHeader("Authorization");
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+
+            String token = authHeader.substring(7);
+
+            try {
+                String username = jwtUtil.extractUsername(token);
+
+                System.out.println("Valid token for user: " + username);
+
+            } catch (Exception e) {
+                ((HttpServletResponse) response).setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                return;
+            }
+        }
+
+        chain.doFilter(request, response);
+    }
+}
